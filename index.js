7967ef7d94fed34275a8ee02c7103aebef7d6e93
Beth Schofield <bethmschofield@gmail.com>
2017-12-17 08:27:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ var recipes = {
 };
 
 function updateObjectWithKeyAndValue(object, key, value){
-  object[key] = value;
-  return object;
-}
-
-function updateObjectWithKeyAndValue(object, key, value){
   var newRecipe = Object.assign({}, object)
   newRecipe[key] = value;
   return newRecipe
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
 }
